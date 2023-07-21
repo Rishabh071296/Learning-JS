55c9aed812b0c4323304319e01b5e39ace32455c
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-07-21 13:24:22 +0530

    update

---
+++ Basics/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+//alert ( 3+ 3 ) //we are using nodejs,not browser
+
+let name = "Rishabh"
+let age = 19
+let isloggedIn = true
+
+//number
+//bigint
+//string => "",''.
+//boolean => true/false
+//null=> standalone value
+//undifined => value not defined
+//symbol=> unique.
+
+//object
+
+console.log(typeof undefined);//undifined
+console.log(typeof null);//object
