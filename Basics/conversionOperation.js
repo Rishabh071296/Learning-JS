23697252964aadaa8fc14c368572d0c97a20faf8
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-07-21 17:05:13 +0530

    update

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,27 @@
+//++++++++++++++++++++++++++++++++Conversion++++++++++++++++++++++++++++++++++++
+
+let score = "3369jdcn"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"77" => 77
+//"33abcdf" =>NAN (not a number)
+//true=>1;false=>0
+
+//+++++++++++++++++++++++++++++++++++Operations+++++++++++++++++++++++++++++++++++++++++++
+
+let str1 = "Hello"
+let str2 = " Rishabh Singh"
+let Str3= str1+str2
+console.log(Str3);
+
+// console.log("1"+2); => output = 12
+// console.log( 1+"2"); => output = 12
+// console.log("1"+2+2); => output = 122
+// console.log(1+2+"2"); => output = 32
