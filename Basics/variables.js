71fd314bff4cc033acf25ef0e560f96cc5b3a673
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-07-21 13:01:25 +0530

    Update

---
+++ Basics/variables.js
@@ -0,0 +1,22 @@
+const accountID = 1415451
+let accountEmail = "Rishi@xyz.com"
+var accountPassword = "123654"
+ accountCity= "Pratapgarh"
+ 
+
+
+//accountID = 2// not allowed in const variable 
+
+// console.log(accountID);
+
+/*
+Prefer not to use var
+because of issurin block scope and functional scope 
+*/
+
+// accountEmail = "xyz@.com"
+// accountPassword = "987456"
+// accountCity = "Delhi NCR"
+
+
+console.table([accountID,accountEmail,accountPassword,accountCity]);
