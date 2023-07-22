e6778cac8925bf8af301e68dc6fd4b4699505d5c
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-07-22 18:54:51 +0530

    Update

---
+++ Basics/Dates.js
@@ -0,0 +1,25 @@
+//Dates (object)
+
+let myDates = new Date()
+console.log(myDates.toString());
+console.log(typeof myDates);
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
+let myCreatedDate = new Date ("01-08-2023")
+console.log(myCreatedDate.getTime());
+
+console.log(Math.floor(Date.noe()/1000)); // Interview related Important ++++++++++
+
+//`${newDate.grtDay()} and the time`
+
+newDate.toLocalString('default',{
+weekday:"long"
+
+})
+
+
+
+
+
