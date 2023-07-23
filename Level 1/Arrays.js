fa3787ef46c5d1c740e2675aa184858996a3abad
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-07-23 13:00:32 +0530

    Update

---
+++ Level 1/Arrays.js
@@ -0,0 +1,35 @@
+//Arrays++++++++++++++++++++++++++++++++++
+// const myArr = [0,1,2,3,4,5,6]
+// const myHeros = ["Ram","Shyam"]
+
+// const myArr2 = new Array (1,2,3,4)
+// console.log(myArr[1]);
+
+//Arrays methods+++++++++++++++++++++
+
+// myArr.push(6)//Add new value of elements
+// myArr.push(7)
+// myArr.pop() // Remove the last value of array
+// console.log(myArr);
+
+// console.log(myArr.includes(9));//add a new value is starting of elements
+// console.log(myArr.indexOf(3));// show the length of index.
+
+//slice and splice methods+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log("A ",myArr);
+// const myn1 = myArr.slice(1,3)
+
+// console.log(myn1);
+// console.log("B",myArr);
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// const arrays=[1,2,3,[4,5,8],1,0,[4,5,[9,7]]]
+// const another_array  = arrays.flat(Infinity)
+// console.log(another_array);
+// console.log(typeof(another_array));
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+let score1 = 1000
+let score2 = 1400
+let score3 = 1854
+console.log(Array.of(score1,score2,score3));
+
