367e01a1031db0fa041b5cdebedae20e99e72fb4
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-07-24 12:45:03 +0530

    Update

--- Level 1/object.js
+++ Level 1/object.js
@@ -25,9 +25,41 @@ jsUser.email = "rishabh@google.com"
 jsUser.greeting = function(){
     console.log("Hello JS user");
 }
-console.log(jsUser.greeting());
+// console.log(jsUser.greeting());
 
 jsUser.greetingTwo = function(){
     console.log(`Hello JS user,${this.name}`);
 }
-console.log(jsUser.greetingTwo());
+// console.log(jsUser.greetingTwo());
+
+//*****************************Part-2************************************************************************/
+
+
+
+// const RS7user= new Object();++++ Singelton Object
+// const RS7user = {} +++++ Non singelton Object
+
+// console.log(RS7user);
+
+const regularUser={
+    email: "xyz@gmail.com",
+    fullname:{
+        userfullname:{
+        Firstname:"Rishabh",
+        lastname: "Singh"
+    }
+}
+}
+
+// console.log(regularUser.fullname);
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+
+// const obj3 ={obj1,obj2}********************1st way****************
+// const obj3= Object.assign({},obj1,obj2)********************2nd Way*********************
+// const obj3= {...obj1, ...obj2}***************3rd way (important)***********
+// console.log(obj3);
+
+console.log(Object.keys(regularUser));
+console.log(Object.values(regularUser));
