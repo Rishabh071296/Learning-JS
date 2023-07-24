69a0f1e96fecb605562da2b27a281e9b01543e28
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-07-24 16:59:29 +0530

    Update

---
+++ Level 2/Function.js
@@ -0,0 +1,30 @@
+// function addTwoNumber(number1,number2) {
+//     console.log(number1+number2);
+// }
+
+function addTwoNumber(number1,number2) {
+    // let result = number1+number2
+    // return result
+    return number1+number2
+
+}
+const result =addTwoNumber(7,7)
+
+// console.log("Result: ",result);
+
+
+function loginUserMessage(username) {
+    if (username===undefined) {
+        console.log("Please enter a username");
+        return
+        
+    }
+
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Rishabh"));
+// console.log(loginUserMessage());
+
+
+
