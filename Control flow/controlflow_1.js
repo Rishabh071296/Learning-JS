33812c0475546dbd6b7859c6fef745f67ff33a17
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-07-25 22:26:54 +0530

    update

---
+++ Control flow/controlflow_1.js
@@ -0,0 +1,40 @@
+//IF
+
+if (true) {
+    // (true) then excuted code otherwise not
+}
+// == is show the equal the value and === is show that value + data type 
+
+// const score = 200
+//  if (score > 100) {
+//     let power = "fly"
+//     console.log(`user power: ${power}`);
+//  }
+//  console.log(`user power ${power}`); //***if you are using var then code will be excuted  */
+
+//Short hand notastion+++++++++++++++++++++++++++++++++++++++++++
+
+const balance = 1000
+
+// if (balance< 500) {
+//     console.log("less than 500");
+// } else if(balance<750){
+//     console.log("less than 750");
+// }else if(balance<950){
+//     console.log("less than 950");
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard =true
+const LoggedInFromGoogle = false
+const LoggedInFromEmail = true
+if (userLoggedIn && debitCard) {
+    console.log("Allow to buy course");
+}
+// **************|| pipes symbol to compair together which one is true or match**************************
+if (LoggedInFromEmail || LoggedInFromGoogle) {
+    console.log("user logged in");
+}
